3efe0255cd079df851f1543fa6f5a28b921d32ac
yanevannn <yanevan03@gmail.com>
2025-03-09 15:29:45 +0800

    Refactor AuthController: improve registration methods with validation and error handling

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,42 +2,73 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    function login(){
+    function login()
+    {
         if (Auth::check()) {
             return redirect()->route('dashboard');
         }
         return view('auth.login');
     }
 
-    function doLogin(Request $request){
-       $data =[
-              'email' => $request->email,
-              'password' => $request->password
-       ];
+    function doLogin(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
 
-       if(Auth::attempt($data)){
+        if (Auth::attempt($data)) {
 
-           return redirect()->route('dashboard');
-
-         }else{
-              return redirect()->route('login')->withErrors('Email dan Password salah !');
-            }
+            return redirect()->route('dashboard');
+        } else {
+            return redirect()->route('login')->withErrors('Email dan Password salah !');
+        }
     }
 
-    function register(){
+    function register()
+    {
         return view('auth.register');
     }
 
-    function doRegister(Request $request){
-        dd($request->all());
+    function doRegister(Request $request)
+    {
+        $request->validate(
+            [
+                'name' => 'required|string|max:255',
+                'email' => 'required|email|email:rfc,dns|unique:users,email',
+                'password' => [
+                    'required',
+                    'string',
+                    'min:8',
+                    'regex:/[A-Z]/',       // Harus ada huruf besar
+                    'regex:/[a-z]/',       // Harus ada huruf kecil
+                    'regex:/[0-9]/',       // Harus ada angka
+                    'regex:/[@$!%*?&#]/',  // Harus ada karakter spesial
+                ],
+            ]
+        );
+
+        $data = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'role' => 'user'
+        ];
+
+        User::create($data);
+
+        return redirect()->route('login')->withErrors('Registrasi berhasil, silahkan login !');
+
     }
 
-    function logout(){
+    function logout()
+    {
         Auth::logout();
         return redirect()->route('login');
     }
--- resources/views/components/register-form.blade.php
+++ resources/views/components/register-form.blade.php
@@ -1,153 +1,87 @@
 <div class="max-w-md w-full p-6" id="registerForm">
     <h1 class="text-3xl font-semibold mb-6 text-black text-center">Pendaftaran Akun</h1>
-    <h2 class="text-sm text-gray-500">Sudah memiliki akun ? <a href="{{ route('login') }}" class="text-blue-600 hover:underline hover:text-blue-700 font-bold">Masuk</a></h2>
+    <h2 class="text-sm text-gray-500">Sudah memiliki akun ? <a href="{{ route('login') }}"
+            class="text-blue-600 hover:underline hover:text-blue-700 font-bold">Masuk</a></h2>
 
     <form action="{{ route('register') }}" method="POST" class="space-y-4">
         @csrf
-        <!-- Step 1 -->
-        <div id="step1">
-            <div class="space-y-4">
-                <div>
-                    <label for="nik" class="block text-sm font-bold text-gray-700">NIK</label>
-                    <input type="number" id="nik" name="nik"
-                        class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
-                        placeholder="Masukkan NIK" required>
-                    <p id="nikError" class="text-red-500 text-xs mt-1 hidden">NIK wajib diisi!</p>
-                </div>
-
-                <div>
-                    <label for="nama" class="block text-sm font-bold text-gray-700">Nama Lengkap</label>
-                    <input type="text" id="nama" name="nama"
-                        class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
-                        placeholder="Masukkan Nama Lengkap" required>
-                    <p id="namaError" class="text-red-500 text-xs mt-1 hidden mb-0">Nama lengkap wajib diisi!</p>
-                </div>
+        <div class="space-y-4">
+            <div>
+                <label for="name" class="block text-sm font-bold text-gray-700">Nama Lengkap</label>
+                <input type="text" id="name" name="name"
+                    class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
+                    placeholder="Masukkan Nama Lengkap" required>
             </div>
             <div>
-                <button type="button" id="nextStep"
-                    class="mt-8 w-full bg-blue-600 text-white p-2 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300">Berikutnya</button>
+                <label for="email" class="block text-sm font-bold text-gray-700">Email</label>
+                <input type="email" id="email" name="email"
+                    class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
+                    placeholder="Masukkan email" required>
             </div>
-            <p class="text-gray-500 text-sm text-justify mt-4"> Dengan melakukan pendaftaran, saya setuju dengan 
-                <a href="" class="text-blue-600 hover:underline hover:text-blue-700 font-semibold">Kebijakan Privasi dan Syarat & Ketentuan</a> WTC 2 Japan.
-            </p>
+            <div>
+                <label for="password" class="block text-sm font-bold text-gray-700">Password</label>
+                <input type="password" id="password" name="password"
+                    class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
+                    placeholder="Masukkan password">
+                <p class="text-sm mt-2 text-gray-700">Password harus memenuhi kriteria berikut : </p>
+                <ul class="mt-2 text-xs text-gray-500">
+                    <li id="length" class="flex items-center text-gray-500">
+                        <svg class="inline-block w-4 h-4 mr-2 text-gray-500" fill="none" stroke="currentColor"
+                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+                            </path>
+                        </svg>
+                        Minimal 8 karakter
+                    </li>
+                    <li id="uppercase" class="flex items-center text-gray-500">
+                        <svg class="inline-block w-4 h-4 mr-2 text-gray-500" fill="none" stroke="currentColor"
+                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+                            </path>
+                        </svg>
+                        Huruf besar (A-Z)
+                    </li>
+                    <li id="lowercase" class="flex items-center text-gray-500">
+                        <svg class="inline-block w-4 h-4 mr-2 text-gray-500" fill="none" stroke="currentColor"
+                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+                            </path>
+                        </svg>
+                        Huruf kecil (a-z)
+                    </li>
+                    <li id="number" class="flex items-center text-gray-500">
+                        <svg class="inline-block w-4 h-4 mr-2 text-gray-500" fill="none" stroke="currentColor"
+                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+                            </path>
+                        </svg>
+                        Angka (1 sampai 9)
+                    </li>
+                    <li id="special" class="flex items-center text-gray-500">
+                        <svg class="inline-block w-4 h-4 mr-2 text-gray-500" fill="none" stroke="currentColor"
+                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+                            </path>
+                        </svg>
+                        Karakter spesial (!@#$%^&*(),.?":{}|<>)
+                    </li>
+                </ul>
+            </div>
         </div>
-
-        <!-- Step 2 -->
-        <div id="step2" class="hidden">
-            <div class="space-y-4">
-                <div>
-                    <label for="email" class="block text-sm font-bold text-gray-700">Email</label>
-                    <input type="email" id="email" name="email"
-                        class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300" placeholder="Masukkan email" required>
-                </div>
-                <div>
-                    <label for="phone" class="block text-sm font-bold text-gray-700">No HP</label>
-                    <input type="text" id="phone" name="phone"
-                        class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300" placeholder="Masukkan No HP" required>
-                </div>
-                <div>
-                    <label for="password" class="block text-sm font-bold text-gray-700">Password</label>
-                    <input type="password" id="password" name="password"
-                        class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
-                        placeholder="Masukkan password">
-                    <p class="text-sm mt-2 text-gray-700">Password harus memenuhi kriteria berikut : </p>
-                    <ul class="mt-2 text-xs text-gray-500">
-                        <li id="length" class="flex items-center text-gray-500">
-                            <svg class="inline-block w-4 h-4 mr-2 text-gray-500" fill="none" stroke="currentColor"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M5 13l4 4L19 7">
-                                </path>
-                            </svg>
-                            Minimal 8 karakter
-                        </li>
-                        <li id="uppercase" class="flex items-center text-gray-500">
-                            <svg class="inline-block w-4 h-4 mr-2 text-gray-500" fill="none" stroke="currentColor"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M5 13l4 4L19 7">
-                                </path>
-                            </svg>
-                            Huruf besar (A-Z)
-                        </li>
-                        <li id="lowercase" class="flex items-center text-gray-500">
-                            <svg class="inline-block w-4 h-4 mr-2 text-gray-500" fill="none" stroke="currentColor"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M5 13l4 4L19 7">
-                                </path>
-                            </svg>
-                            Huruf kecil (a-z)
-                        </li>
-                        <li id="number" class="flex items-center text-gray-500">
-                            <svg class="inline-block w-4 h-4 mr-2 text-gray-500" fill="none" stroke="currentColor"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M5 13l4 4L19 7">
-                                </path>
-                            </svg>
-                            Angka (1 sampai 9)
-                        </li>
-                        <li id="special" class="flex items-center text-gray-500">
-                            <svg class="inline-block w-4 h-4 mr-2 text-gray-500" fill="none" stroke="currentColor"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M5 13l4 4L19 7">
-                                </path>
-                            </svg>
-                            Karakter spesial (!@#$%^&*(),.?":{}|<>)
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div>
-                <button type="submit" id="registerButton" class="mt-5 w-full bg-gray-400 text-white p-2 rounded-md focus:outline-none transition-colors duration-300" disabled>Daftar Sekarang</button>
-            </div>
+        <div>
+            <button type="submit" id="registerButton"
+                class="mt-5 w-full bg-gray-400 text-white p-2 rounded-md focus:outline-none transition-colors duration-300"
+                disabled>Daftar Sekarang</button>
         </div>
     </form>
+    <p class="text-gray-500 text-sm text-justify mt-4"> Dengan melakukan pendaftaran, saya setuju dengan
+        <a href="" class="text-blue-600 hover:underline hover:text-blue-700 font-semibold">Kebijakan Privasi dan
+            Syarat & Ketentuan</a> WTC 2 Japan.
+    </p>
 </div>
 
 
 <script>
-    document.getElementById('nextStep').addEventListener('click', function() {
-        // Mengambil nilai input dan menghapus spasi kosong di awal & akhir
-        const nik = document.getElementById('nik').value.trim();
-        const nama = document.getElementById('nama').value.trim();
-
-        // Ambil elemen error
-        const nikError = document.getElementById('nikError');
-        const namaError = document.getElementById('namaError');
-
-        let valid = true; // Flag untuk validasi
-
-        // Cek NIK
-        if (nik === '') {
-            nikError.classList.remove('hidden'); // Tampilkan error
-            document.getElementById('nik').classList.add('border-red-500'); // Tambah efek merah pada input
-            valid = false;
-        } else {
-            nikError.classList.add('hidden'); // Sembunyikan error
-            document.getElementById('nik').classList.remove('border-red-500');
-        }
-
-        // Cek Nama
-        if (nama === '') {
-            namaError.classList.remove('hidden');
-            document.getElementById('nama').classList.add('border-red-500');
-            valid = false;
-        } else {
-            namaError.classList.add('hidden');
-            document.getElementById('nama').classList.remove('border-red-500');
-        }
-
-        // Jika valid, lanjut ke Step 2
-        if (valid) {
-            document.getElementById('step1').classList.add('hidden');
-            document.getElementById('step2').classList.remove('hidden');
-        }
-    });
-
-
     document.getElementById('password').addEventListener('input', function() {
         const password = this.value;
         const length = document.getElementById('length');
@@ -225,12 +159,12 @@
 
     // Fungsi untuk mengecek apakah semua input sudah terisi
     function checkFormValidity() {
+        const name = document.getElementById('name').value.trim();
         const email = document.getElementById('email').value.trim();
-        const phone = document.getElementById('phone').value.trim();
         const password = document.getElementById('password').value.trim();
         const registerButton = document.getElementById('registerButton');
 
-        if (email !== '' && phone !== '' && isPasswordValid(password)) {
+        if (name !== '' && email !== '' && isPasswordValid(password)) {
             registerButton.disabled = false;
             registerButton.classList.remove('bg-gray-400');
             registerButton.classList.add('bg-blue-600', 'hover:bg-blue-700');
@@ -243,7 +177,6 @@
 
     // Event listener untuk memantau perubahan pada input
     document.getElementById('email').addEventListener('input', checkFormValidity);
-    document.getElementById('phone').addEventListener('input', checkFormValidity);
     document.getElementById('password').addEventListener('input', function() {
         checkFormValidity();
     });
