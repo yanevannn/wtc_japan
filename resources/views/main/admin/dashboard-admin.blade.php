bc48a71be8aa2f0db455a1ab2cc175826a096ca1
yanevannn <yanevan03@gmail.com>
2025-05-18 23:32:43 +0800

    update: adding route on component dashboard card

--- resources/views/main/admin/dashboard-admin.blade.php
+++ resources/views/main/admin/dashboard-admin.blade.php
@@ -1,11 +1,38 @@
 <x-main-layout>
     <x-slot:title>Dashboard</x-slot:title>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <x-dashboard-card title="Jumlah Gelombang" value="{{ $data['jumlah_gelombang'] }}" :icon="file_get_contents(public_path('assets/verfikasi.svg'))" />
-        <x-dashboard-card title="Jumlah Pendaftar Gelombang terbaru" value="{{ $data['jumlah_pendaftar_terbaru'] }}" :icon="file_get_contents(public_path('assets/verfikasi.svg'))" />
-        <x-dashboard-card title="Jumlah Angkatan" value="{{ $data['jumlah_angkatan'] }}" :icon="file_get_contents(public_path('assets/verfikasi.svg'))" />
-        <x-dashboard-card title="Verifikasi Dokumen" value="{{ $data['verif_dokumen'] }}" :icon="file_get_contents(public_path('assets/verfikasi.svg'))" />
-        <x-dashboard-card title="Verifikasi Pembayaran Pendaftaran" value="{{ $data['verif_pembayaran_pendaftaran'] }}" :icon="file_get_contents(public_path('assets/verfikasi.svg'))" />
-        <x-dashboard-card title="Verifikasi Pembayaran Pelatihan" value="{{ $data['verif_pembayaran_pelatihan'] }}" :icon="file_get_contents(public_path('assets/verfikasi.svg'))" />
+        <x-dashboard-card 
+        route="{{ route('gelombang.index') }}" 
+        title="Jumlah Gelombang" 
+        value="{{ $data['jumlah_gelombang'] }}" 
+        :icon="file_get_contents(public_path('assets/verfikasi.svg'))" />
+
+        <x-dashboard-card 
+        title="Jumlah Pendaftar Gelombang terbaru" 
+        value="{{ $data['jumlah_pendaftar_terbaru'] }}"
+        :icon="file_get_contents(public_path('assets/verfikasi.svg'))" />
+
+        <x-dashboard-card 
+        route="{{ route('angkatan.index') }}" 
+        title="Jumlah Angkatan" 
+        value="{{ $data['jumlah_angkatan'] }}" 
+        :icon="file_get_contents(public_path('assets/verfikasi.svg'))" />
+
+        <x-dashboard-card 
+        title="Verifikasi Dokumen" 
+        route="{{ route('verifikasi.dokumen.index') }}"
+        value="{{ $data['verif_dokumen'] }}" 
+        :icon="file_get_contents(public_path('assets/verfikasi.svg'))" />
+
+        <x-dashboard-card 
+        title="Verifikasi Pembayaran Pendaftaran"
+        route="{{ route('verifikasi.pembayaran-pendaftaran.index') }}"
+        value="{{ $data['verif_pembayaran_pendaftaran'] }}" 
+        :icon="file_get_contents(public_path('assets/verfikasi.svg'))" />
+
+        <x-dashboard-card 
+        title="Verifikasi Pembayaran Pelatihan" 
+        value="{{ $data['verif_pembayaran_pelatihan'] }}"
+        :icon="file_get_contents(public_path('assets/verfikasi.svg'))" />
     </div>
 </x-main-layout>
